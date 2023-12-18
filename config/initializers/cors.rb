01b08b2514136bcb8dc099de9ad2b8203f55fb6b
Zak Schenck <zakschenck@MacBook-Pro.localdomain>
2023-12-18 16:41:47 -0500

    cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://zakschenck.github.io/spyspace-frontend/"
+    origins "https://zakschenck.github.io/spyspace-frontend"
 
     resource "*",
       headers: :any,
